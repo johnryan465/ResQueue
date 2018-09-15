dc5609fe9ff37f4470822aa51cb30ddb681a4304
CianLR <cian.ruane1@gmail.com>
2018-09-15 17:15:48 -0400

    Add main.py

---
+++ backend/libroute/libroute.py
@@ -0,0 +1,27 @@
+from util import Point, Solution
+
+def get_routes(start, people, vehicle_sizes):
+    pass
+
+# def euclidian(a, b):
+#     return ((a[0] - b[0]) ** 2) + ((a[1] - b[1]) ** 2)
+# 
+# def shortest_route(start, points):
+#     pass
+# 
+# def rescue_clusters(max_sizes, points, dist=euclidian):
+#     pts = points[:]
+#     clusters = []
+#     for sz in max_sizes:
+#         clust = []
+#         if pts:
+#             u = pts.pop()
+#             sz -= 1
+#             clust.append(u)
+#             pts = sorted(pts, key=lambda a: dist(a, u), reverse=True)
+#             while pts and sz > 0:
+#                 clust.append(pts.pop())
+#                 sz -= 1
+#         clusters.append(clust)
+#     return clusters
+
---
+++ backend/main.py
@@ -0,0 +1,9 @@
+from libroute import libroute
+# Import main middleware thing
+
+libroute.get_routes(None, None, None)
+
+if __name__ == '__main__':
+    # Start the server
+    pass
+
