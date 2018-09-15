120530ae01eacd7c798c04b6be892977266987ea
johnryan465 <johnryan465@gmail.com>
2018-09-15 18:58:44 -0400

    Added Azure

--- backend/middleware/main.py
+++ backend/middleware/main.py
@@ -1,34 +1,39 @@
-from flask import Flask
-from tinydb import TinyDB, Query
+from flask import Flask, request
+from pymongo import MongoClient
 import datetime
-Point = Query()
-points_db = TinyDB('points_db.json')
-vehicles_db = TinyDB('vehicles_db.json')
+import json
+client = MongoClient("mongodb://resqueue:xhCZluztAHMaBa5bhpyBOdizbbdZ9MpzIB86XFFW4heqazNsXud9544V64ASltl7WtTZlRj1vFpgpprKkBVMJA==@resqueue.documents.azure.com:10255/?ssl=true&replicaSet=globaldb")
+db = client.resqueue    #Select the database
 
+
+people_table = db.people
+vehicles_table = db.vehicles
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+    return "Hello World!"
 
 @app.route('/api/points', methods = ['GET','POST'])
-def points():
+def people():
     if request.method == 'POST':
         data = request.form
-        return points_db.insert({'lat': data['lat'], 'long': data['long'], 'priority': data['priority'], 'note': data['note'], 'status':0,'time':datetime.datetime.now()})
+        return people_table.insert({'location': [data['lat'],data['long'] ], 'priority': data['priority'], 'note': data['note'], 'status':0,'time':str(datetime.datetime.now())})
     if request.method == 'GET':
-        return points_db.all()
-
+        l = []
+        for person in people_table.find():
+            l.append(str(person))
+        return json.dumps(l)
 @app.route('/api/vehicles', methods = ['GET','PUT','POST'])
 def vehicles():
     if request.method == 'POST':
         data = request.form
-        return vehicles_db.insert({'name': data['name'], 'size': data['size'], 'quantity': data['quantity']})
+        return vehicles_table.insert({'name': data['name'], 'size': data['size'], 'quantity': data['quantity']})
     if request.method == 'GET':
-        return vehicles_db.all()
+        return vehicles_table.find()
     if request.method == 'PUT':
         data = request.form
-        return vehicles_db.update({'name': data['name'], 'size': data['size'], 'quantity': data['quantity']},doc_ids=[data[id]])
+        return vehicles_db.update({'_id': ObjectId(data[id])}, {'name': data['name'], 'size': data['size'], 'quantity': data['quantity']})
 
 
 def get_routes():
