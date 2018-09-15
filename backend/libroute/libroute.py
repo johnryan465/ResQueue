78ef296b5d47593cdfb730c1a795e94489903a21
CianLR <cian.ruane1@gmail.com>
2018-09-15 19:30:50 -0400

    Add TSP and move UF

--- backend/libroute/libroute.py
+++ backend/libroute/libroute.py
@@ -1,4 +1,5 @@
-from util import Point, Solution, UF
+from util import Point, Solution
+from libroute import union_find
 
 def eucl(a, b):
     return (((a.lat - b.lat) ** 2) + ((a.lng - b.lng) ** 2)) ** 0.5
@@ -14,7 +15,7 @@ def get_clusters(people, vehicle_sizes, dists):
     vsizes = sorted(vehicle_sizes, reverse=True)
     # Node 0 shall be a special node that indicates
     # the cluster has been assigned to a vehicle.
-    uf = UF(len(people) + 1)
+    uf = union_find.UF(len(people) + 1)
     edges = []
     for i in range(1, len(people) + 1):
         for j in range(i + 1, len(people) + 1):
@@ -28,19 +29,27 @@ def get_clusters(people, vehicle_sizes, dists):
             uf.is_joined(0, v)):
             continue  # Used in a cluster already or would cause a loop
         uf.join(u, v)
-        if len(uf.get_cluster(u)) == vsizes[-1]:
-            clusters.append(tuple(uf.get_cluster(u)))
+        cluster = uf.get_cluster(u)
+        if len(cluster) == vsizes[-1]:
+            clusters.append([people[p - 1] for p in cluster])
             vsizes.pop()
             uf.join(0, u)
-    return clusters
+    unsaved = [people[p - 1] for p in range(1, len(people) + 1) if uf.is_alone(p)]
+    return clusters, unsaved
 
+def traveling_salesman(start, points):
+    from random import shuffle
+    shuffle(points)
+    return [start] + points
 
 def get_routes(start, people, vehicle_sizes):
     for vs in vehicle_sizes:
         if vs < 2:
             return None
     dist = get_dist_matrix([start] + people)
-    return get_clusters(people, vehicle_sizes, dist)
-
-
+    sol = Solution()
+    clusters, sol.unsaved = get_clusters(people, vehicle_sizes, dist)
+    for clust in clusters:
+        sol.routes.append(traveling_salesman(start, clust))
+    return sol
 
