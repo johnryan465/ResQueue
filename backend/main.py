b2f10a9a0c9e4c55aeba9c3d7f58a6c7ebfc7044
johnryan465 <johnryan465@gmail.com>
2018-09-15 17:26:10 -0400

    Updated backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from libroute import libroute
 # Import main middleware thing
-
+from middleware import main
 libroute.get_routes(None, None, None)
 
 if __name__ == '__main__':
+    main.app.run(host= '0.0.0.0',port = 8000)
     # Start the server
     pass
-
