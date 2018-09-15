78543b3aab3d1bb51ae683e2502e1a5f7dffaec3
CianLR <cian.ruane1@gmail.com>
2018-09-15 17:48:56 -0400

    Add Union Find to utils

--- backend/util.py
+++ backend/util.py
@@ -9,3 +9,27 @@ class Solution:
         self.unmatched = unmatched  # list[Point]
         self.routes = routes  # list[list[Point]]
 
+class UF:
+    def __init__(self, N):
+        self.parent = list(range(N))
+        self.size = [1] * N
+
+    def _get_root(self, i):
+        while i != self.parent[i]:
+            self.parent[i] = self.parent[self.parent[i]]
+            i = self.parent[i]
+        return i
+
+    def is_joined(self, a, b):
+        return self._get_root(a) == self._get_root(b)
+
+    def join(self, a, b):
+        a, b = self._get_root(a), self._get_root(b)
+        if a == b:
+            return
+        if self.size[a] < self.size[b]:
+            a, b = b, a
+        self.parent[b] = a
+        self.size[a] += self.size[b]
+
+
